be181c79c024aefb1430975f1e49886fcee7f2ed
harris4got <harris.reid@rocketmail.com>
2019-09-07 23:31:03 +0530

    Initial Committ

---
+++ RS4.py
@@ -0,0 +1,94 @@
+from gi.repository import GObject, RB, Peas
+from re import search
+import S4
+
+class RS4 (GObject.Object, Peas.Activatable):
+	object = GObject.property(type=GObject.Object)
+	__gtype_name = 'RS4'
+
+	def __init__(self):
+		super(RS4, self).__init__()
+
+	def do_activate(self):
+		print("RadioScrobbler4 is Active!")
+		while S4.get_session() is None:
+			self.session=S4.get_session()
+		self.shell = self.object
+		self.player = self.shell.props.shell_player
+		self.pc_id = self.player.connect('playing-changed', self.playing_changed)
+		self.psc_id = self.player.connect('playing-song-changed', self.playing_song_changed)
+		self.pspc_id = self.player.connect('playing-song-property-changed', self.playing_song_property_changed)
+		self.current_entry=self.old_title=self.current_location=self.current_radio=self.current_title=None  #Del in deactivate check
+		if self.player.get_playing()[1]:	#If a track is already playing while the plugin is activated, set entry
+			print ("Track is already playing")
+			self.set_entry(self.player.get_playing_entry())
+
+	def do_deactivate(self):
+		self.player.disconnect(self.pc_id)
+		self.player.disconnect(self.psc_id)
+		self.player.disconnect(self.pspc_id)
+		self.current_entry=self.old_title=self.current_location=self.current_radio=self.current_title=None
+		self.player = None
+		del self.shell
+
+	def playing_changed(self,player,playing):
+		if playing:
+			self.set_entry(player.get_playing_entry())
+			print ("Playing")
+		else:
+			self.current_entry= None
+			print("Not Playing")
+
+	def playing_song_changed(self,player,entry):
+		if player.get_playing()[1]:
+			self.set_entry(entry)
+			print("Playing Song Changed")
+
+	def playing_song_property_changed(self, player, uri,property, old, new):
+		if player.get_playing()[1] and property in ('title', 'rb:stream-song-title'):
+			print("Playing Props Changed")
+			self.current_title=new
+			self.set_status()
+
+	def set_entry(self,entry):		#Set current entry from new entry
+		if entry==self.current_entry:   #If entry is the same as the current one
+			return
+		self.current_entry=entry	#Set new entry as the current one
+		if entry is None:			#Entry is none when track is not playing
+			return
+		self.set_status_from_entry()
+
+	def set_status_from_entry(self):		#Sets status from the current entry
+		db=self.shell.get_property("db")
+		self.current_location=self.current_entry.get_string(RB.RhythmDBPropType.LOCATION)   #File Location, which means url for a radio entry
+		if not search('^file', self.current_location):			#If not playing a file then it must be radio url!
+			self.current_radio=self.current_entry.get_string(RB.RhythmDBPropType.TITLE)   #Name of the station
+			genre=self.current_entry.get_string(RB.RhythmDBPropType.GENRE)
+			self.current_title=db.entry_request_extra_metadata(self.current_entry, 'rb:stream-song-title')
+			print(bcolors.HEADER+"Radio: %s Genre: %s Location: %s"%(self.current_radio,genre,self.current_location)+bcolors.ENDC)
+			self.set_status()
+
+	def set_status(self):				#Sets status from current title
+		if self.old_title!=self.current_title and self.current_title:   #Make sure track name changed and it is not Null
+			print(bcolors.HEADER+"♫ %s ♫ "%(self.current_title)+bcolors.ENDC)
+			#self.core()
+			self.old_title=self.current_title		#Sets current title as old title
+
+class bcolors:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	BADBLUE = '\033[96m'
+	GREY = '\033[90m'
+	ENDC = '\033[0m'
+
+	def disable(self):
+		self.HEADER = ''
+		self.OKBLUE = ''
+		self.OKGREEN = ''
+		self.WARNING = ''
+		self.FAIL = ''
+		self.ENDC = ''	
+
---
+++ S4.py
@@ -0,0 +1,140 @@
+﻿import urllib.request, urllib.parse, urllib.error, re, html, webbrowser, hashlib, os
+from re import search
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk as gtk
+
+LOC=os.environ.get('HOME')+'/.local/share/rhythmbox/plugins/RS4/'
+SESSION_FILE=LOC+'.session'
+THE_API_FILE=LOC+'.api'
+FILE={'session':SESSION_FILE}
+
+def get_api(option):
+	try:
+		stream=open(THE_API_FILE,"r")
+		if option==0:
+			API_KEY=search('API_KEY=(.+)',stream.read()).group(1)
+			stream.close();return(API_KEY)
+		if option==1:
+			API_AD=search('API_AD=(.+)',stream.read()).group(1)
+			stream.close();return(API_AD)
+	except:
+		print("API details could not be found")
+		return None
+
+DEF_CAB={'artist':'<artist>(.+)</artist>','title':'<title>(.+)</title>','duration':'<duration>(\d+)</duration>','album':'<album>(.+)</album>'}
+API_URL = 'http://ws.audioscrobbler.com/2.0/'
+API_KEY = get_api(0)
+API_AD = get_api(1)
+DIRS=dir();exec('p0='+DIRS[0]);exec('p1='+DIRS[1])
+
+
+def record(string,method):				#Logging/Archiving
+	log=open(FILE[method],"a+")
+	log.writelines(string)
+	log.close()
+
+def xmllight(string,cab=DEF_CAB):			#Indigenous XML Parser
+	for x in cab:
+		t=search(str(cab[x]),str(string))
+		if t:
+			cab[x]=t.group(1)
+			if '&' in str(cab[x]):
+				cab[x]=html.unescape(str(cab[x]))
+		else:
+			cab[x]=None	
+
+def process(code):					#String Processing
+	output=""
+	i=0
+	while i<code.count('')-1:
+		if code[i]=="1":
+			output+=chr(int(code[i:i+3]))
+			i+=3
+		else:
+			output+=chr(int(code[i:i+2]))
+			i+=2
+	return output	
+
+
+def get_session():					#Get Session Key
+	try:
+		stream=open(SESSION_FILE,"r")
+		sk=search('session_key=(.+)',stream.read()).group(1)
+		stream.close()
+	except:
+		p3=process(p1);exec('global '+DIRS[1]+";"+DIRS[1]+"=p3")
+		parameters={'api_key':API_KEY,'method':"auth.getToken"}
+		param=get_sig(parameters)
+		doc=get_doc(param)
+		if not doc:
+			return None
+		cab={'token':'<token>(.+)</token>'}
+		xmllight(doc,cab)
+		if not cab['token']:
+			print("Error getting token from Last FM")
+			return None
+		authurl='http://www.last.fm/api/auth/?api_key=%s&token=%s'%(API_KEY,str(cab['token']))
+		print(authurl)
+		try:
+			webbrowser.open(authurl,new=2,autoraise=True)
+		except:
+			print("Error opening the auth url")
+			return None
+		dialog = gtk.Dialog("Acceptance", None, 0,(gtk.STOCK_OK, gtk.ResponseType.OK))
+		label=gtk.Label("After you grant permission in browser, click OK")
+		label.show()
+		dialog.get_content_area().add(label)
+		response = dialog.run()
+		dialog.destroy()
+		while gtk.events_pending():
+			gtk.main_iteration()
+		if response == gtk.ResponseType.OK:
+			print ("Accepted")
+		else:
+			return None
+		parameters = { "method" : "auth.getsession", "api_key": API_KEY, "token":str(cab['token']) }
+		param=get_sig(parameters)
+		doc=get_doc(param)
+		if not doc:
+			print("Couldn't get session key from Last FM: Did you click on 'Yes, allow Access'?")
+			return None
+		cab={'name':'<name>(.+)</name>','sk':'<key>(.+)</key>','subscriber':'<subscriber>(\d)</subscriber>'}
+		xmllight(doc,cab)
+		string="[radioscrobbler]\nuser=%s\nsubscriber=%s\nsession_key=%s"%(cab['name'],cab['subscriber'],cab['sk'])
+		record(string,'session')
+		sk=cab['sk']
+	return sk
+
+def get_sig(parameters):				#API Authentication Parameter Builder
+		md5 = hashlib.md5()
+		p2=process(p0);exec('global '+DIRS[0]+";"+DIRS[0]+"=p2")
+		keys = sorted(parameters.keys())
+		keyvalues = []
+		for key in keys:
+				keyvalues.append(str(key))										
+				keyvalues.append(str(parameters[key]))
+
+		sig = ''.join(keyvalues)
+		sig = sig + API_AD
+		try:
+			sig=sig.encode('utf-8')
+		except UnicodeDecodeError:
+			print("Unicode Decode Error")	
+		md5.update(sig)
+		api_sig = md5.hexdigest()
+		parameters["api_sig"] = api_sig
+		return parameters
+
+def get_doc(parameters):				#API Response Reader	 
+		data=urllib.parse.urlencode(parameters)
+		url="%s?%s"%(API_URL,data)
+		try:
+			return urllib.request.urlopen(url).read()
+		except:
+			print("Sorry, couldn't read a valid response from Last FM")
+			return None
+
+
+
+
